e60303dbe6d33482c7fe08577fefba79e6d0ca5c
Ramzi K.M <ramzikm3@gmail.com>
2024-05-09 11:42:43 +0530

    :tada: initial commit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+import cors from 'cors';
+import 'dotenv/config.js';
+import express from 'express';
+import logger from 'morgan';
+import path from 'path';
+// import { connectToDb } from './config/db.connection.js';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+//connect to database
+// connectToDb();
+
+//middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(logger('dev'));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.static(path.join(path.resolve(), 'public')));
+app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true }));
+
+//listening
+app.listen(PORT, () => {
+  console.log(`server started at  http://localhost:${PORT}`);
+});
+
+//route setup
